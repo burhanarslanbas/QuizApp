5cfa29414feeee379d148cf92490c507cecfe7a8
Burhan Arslanbaş <seninyazilimin@outlook.com>
2025-06-13 22:36:42 +0300

    Backend yapısında düzenlemeler yapıldı.

--- Infrastructure/QuizApp.Persistence/Repositories/Question/QuestionReadRepository.cs
+++ Infrastructure/QuizApp.Persistence/Repositories/Question/QuestionReadRepository.cs
@@ -4,7 +4,13 @@ using QuizApp.Persistence.Contexts;
 
 namespace QuizApp.Persistence.Repositories
 {
-    public class QuestionReadRepository : ReadRepository<Question>, IQuestionReadRepository
+        var query = Table.AsQueryable();
+        if (!tracking)
+            query = query.AsNoTracking();
+        return query.Include(q => q.Options);
+    }
+
+    public Task<List<Domain.Entities.Question>> GetByIdsAsync(IEnumerable<Guid> ids, bool tracking = true)
     {
         public QuestionReadRepository(QuizAppDbContext context) : base(context) { }
     }
